7f64be448747a9ee6cc3b5bff6dd30c8d7b43b40
Edward_MIT <usmanoff19980404@gmail.com>
2025-05-03 09:09:09 +0900

    feat: homeNavbar compleated

--- gatget-store-react/src/app/components/headers/HomeNavbar.tsx
+++ gatget-store-react/src/app/components/headers/HomeNavbar.tsx
@@ -6,24 +6,14 @@ export function HomeNavbar () {
 
 
  return (<div className="home-navbar">
-  <Container sx={{mt:"55px", height: "642px"}}>
-    <Stack
-       sx={{height:"50px"}}
-       flexDirection={"row"}
-       justifyContent={"space-between"}
-       alignItems={"center"}
-       >
+  <Container className="navbar-container">
+    <Stack className="menu">
        <Box>
           <NavLink to="/">
-           <img style={{width:"125px", height:"30px"}} src="/icons/burak.svg"/>
+           <img className="brand-logo" src="/icons/logo_beautiful.png"/>
           </NavLink>
        </Box>
-       <Stack
-          flexDirection={"row"}
-          justifyContent={"space-between"}
-          minWidth={"700px"}
-          alignItems={"center"}
-       >
+       <Stack className="links">
          <Box className={"hover-line"}>
            <NavLink to="/" activeClassName={"underline"}>Home</NavLink>
          </Box>
@@ -45,11 +35,42 @@ export function HomeNavbar () {
 
          {!authMember ? (
           <Box>
-            <Button variant="contained" style={{background: "#3776CC", color:"#f8f8ff"}}>Login</Button>
-          </Box>) : (<img/>)}
+            <Button variant="contained" className="login-button">Login</Button>
+          </Box>
+         ) : (
+          <img
+             className="user-avatar"
+             src={"icons/default-user.svg"}
+             aria-haspopup={"true"}
+          />
+         )}
        </Stack>
      </Stack>
-    <Stack> Details</Stack>
+    <Stack className="header-frame">
+      <Stack className={"detail"}>
+        <Box className={"head-main-txt"}>
+        Your Daily Dose of Floral Happiness
+        </Box>
+        <Box className={"wel-txt"}>
+          The Choice, not just a choice
+        </Box>
+        <Box className={"service-txt"}>
+          24 hours service
+        </Box>
+        <Box className={"signup"}>
+          {!authMember ? (
+            <Button
+              variant={"contained"}
+              className={"signup-button"}>
+              SIGN UP
+            </Button>
+          ) : null}
+        </Box>
+      </Stack>
+      <Box className={"logo-frame"}>
+        <div className={"logo-img"}></div>
+      </Box>
+    </Stack>
   </Container>
  </div>);
 }
